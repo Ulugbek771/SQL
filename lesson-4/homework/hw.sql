853112253f5131c27013c7b855ac20a10ab56940
Ulugbek771 <ulugbekberkinov777@gmail.com>
2025-05-21 22:38:26 +0500

    Update hw.sql

--- lesson-4/homework/hw.sql
+++ lesson-4/homework/hw.sql
@@ -41,6 +41,74 @@ FROM Products;
   SELECT DISTINCT Category, ProductName
 FROM Products
 ORDER BY ProductName DESC;
+11.Write a query to select the top 10 products from the Products table, ordered by Price DESC.
 
+  SELECT TOP 10 *
+FROM Products
+ORDER BY Price DESC;
+
+12.Use COALESCE to return the first non-NULL value from FirstName or LastName in the Employees table.
+  SELECT COALESCE(FirstName, Familiya) AS IsmYokiFamiliya
+FROM Xodimlar;
+13.Write a query that selects distinct Category and Price from the Products table.
+  SELECT DISTINCT Turkum, Narx
+FROM Mahsulotlar;
+14.Write a query that filters the Employees table to show employees whose Age is either between 30 and 40 or Department = 'Marketing'.
+  SELECT *
+FROM Xodimlar
+WHERE (Yosh BETWEEN 30 AND 40)
+   OR (Departament = 'Marketing');
+15.Use OFFSET-FETCH to select rows 11 to 20 from the Employees table, ordered by Salary DESC.
+  SELECT *
+FROM Xodimlar
+ORDER BY IshHaqi DESC
+OFFSET 10 ROWS
+FETCH NEXT 10 ROWS ONLY;
+16.Write a query to display all products with Price <= 1000 and Stock > 50, sorted by Stock in ascending order.
+  SELECT *
+FROM Mahsulotlar
+WHERE Narx <= 1000
+  AND Birja > 50
+ORDER BY Birja ASC;
+17.Write a query that filters the Products table for ProductName values containing the letter 'e' using LIKE.
+  SELECT *
+FROM Mahsulotlar
+WHERE ProductName LIKE '%e%';
+18.Use IN operator to filter for employees who work in either 'HR', 'IT', or 'Finance'.
+  SELECT *
+FROM Xodimlar
+WHERE Departament IN ('HR', 'IT', 'Moliya');
+19.Use ORDER BY to display a list of customers ordered by City in ascending and PostalCode in descending order.Use customers table
+SELECT *
+FROM Mijozlar
+ORDER BY Shahar ASC, PochtaIndeksi DESC;
+20.Write a query that selects the top 5 products with the highest sales, using TOP(5) and ordered by SalesAmount DESC.
+  SELECT TOP(5) *
+FROM Mahsulotlar
+ORDER BY SalesAmount DESC;
+21.Combine FirstName and LastName into one column named FullName in the Employees table. (only in select statement)
+SELECT CONCAT(Ism, ' ', Familiya) AS [To'liqIsm]
+FROM Xodimlar;
+22.Write a query to select the distinct Category, ProductName, and Price for products that are priced above $50, using DISTINCT on three columns.
+  SELECT DISTINCT Turkum, ProductName, Narx
+FROM Mahsulotlar
+WHERE Narx > 50;
+23.Write a query that selects products whose Price is less than 10% of the average price in the Products table. (Do some research on how to find average price of all products)
+  SELECT AVG(Narx) AS OrtachaNarx
+FROM Mahsulotlar;
+24.Use WHERE clause to filter for employees whose Age is less than 30 and who work in either the 'HR' or 'IT' department.
+  SELECT *
+FROM Employees
+WHERE Age < 30
+  AND Department IN ('HR', 'IT');
   
+25.Use LIKE with wildcard to select all customers whose Email contains the domain '@gmail.com'.
+  SELECT *
+FROM Mijozlar
+WHERE Email LIKE '%@gmail.com';
+26.Write a query that uses the ALL operator to find employees whose salary is greater than all employees in the 'Sales' department.
+  SELECT *
+FROM Mijozlar
+WHERE Email LIKE '%@gmail.com';
+27.
 
