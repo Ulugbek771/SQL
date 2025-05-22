0098626b04e9a5e1899b6f4a3d26ea9cb6f1c5f6
Ulugbek771 <ulugbekberkinov777@gmail.com>
2025-05-22 19:15:45 +0500

    Update hw.sql

--- lesson-7/homework/hw.sql
+++ lesson-7/homework/hw.sql
@@ -1,1 +1,13 @@
-1.
+1.Write a query to find the minimum (MIN) price of a product in the Products table.
+SELECT MIN(Price) As MinimalPrice 
+from Products;
+2.Write a query to find the maximum (MAX) Salary from the Employees table.
+  CELECT MAX(EmplySalary) AS MaxSalary
+from Employees;
+3.Write a query to count the number of rows in the Customers table.
+  SELECT COUNT(CustID) As CustomerID
+  FROM CUSTOMERS;
+4.Write a query to count the number of unique product categories from the Products table.
+  SELECT COUNT(DISTINCT Category) AS UniqueCategoryCount
+FROM Products;
+5.Write a query to find the total sales amount for the product with id 7 in the Sales table.
