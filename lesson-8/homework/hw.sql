d36c52acda76118128fb1562d322c58c0b6942b3
Ulugbek771 <ulugbekberkinov777@gmail.com>
2025-05-25 13:45:29 +0500

    Update hw.sql

--- lesson-8/homework/hw.sql
+++ lesson-8/homework/hw.sql
@@ -1,1 +1,65 @@
+1.Using Products table, find the total number of products available in each category.
+
+
+
+
+
+
+  
+2.Using Products table, get the average price of products in the 'Electronics' category.
+
+
+
+
+
+
+
+  
+3.Using Customers table, list all customers from cities that start with 'L'.
+
+
+
+
+
+
+
+  
+4.Using Products table, get all product names that end with 'er'.
+
+
+
+
+
+  
+5.Using Customers table, list all customers from countries ending in 'A'.
+
+
+
+
+
+
+  
+6.Using Products table, show the highest price among all products.
+
+
+
+
+
+  
+7.Using Products table, label stock as 'Low Stock' if quantity < 30, else 'Sufficient'.
+
+
+
+
+
+  
+8.Using Customers table, find the total number of customers in each country.
+
+
+
+
+
+
+  
+9.Using Orders table, find the minimum and maximum quantity ordered.
 
